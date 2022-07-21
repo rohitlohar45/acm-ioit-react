43a01215acec65e0a204ae04b7f669aeb65336a9
Aval Sonigara <avals1504@gmail.com>
2022-07-21 21:22:44 +0530

    first

--- src/components/Team/TeamData.jsx
+++ src/components/Team/TeamData.jsx
@@ -1,100 +1,100 @@
-import Aditya from "../../assets/team/Aditya.jpg"
-import Ankit from "../../assets/team/Ankit.jpg"
-import Shruti from "../../assets/team/Shruti.jpg"
-import Sanyukta from "../../assets/team/Sanyukta.jpg"
-import Sameer from "../../assets/team/Sameer.jpg"
-import Vaishnavi from "../../assets/team/Vaishnavi.jpg"
+import Akshita from "../../assets/team/Akshita.jpg"
+import Anurag from "../../assets/team/Anurag.jpeg"
+import Aval from "../../assets/team/Aval.jpeg"
+import Rohit from "../../assets/team/Rohit.jpeg"
+import Saadgi from "../../assets/team/Saadgi.jpg"
+import Yash from "../../assets/team/Yash.jpg"
 import Omkar from "../../assets/team/Omkar.jpg"
-import Hrithik from "../../assets/team/Hrithik.jpg"
-import Shreyas from "../../assets/team/Shreyas.jpg"
-import Ninad from "../../assets/team/Ninad.jpg"
-import Shivprasad from "../../assets/team/Shivprasad.jpg"
-import Abhishek from "../../assets/team/Abhishek.jpg"
+// import Hrithik from "../../assets/team/Hrithik.jpg"
+// import Shreyas from "../../assets/team/Shreyas.jpg"
+// import Ninad from "../../assets/team/Ninad.jpg"
+// import Shivprasad from "../../assets/team/Shivprasad.jpg"
+// import Abhishek from "../../assets/team/Abhishek.jpg"
 
 const teams = [
     {
         id: 1,
-        name: 'A. Shivprasad Rao',
-        role: 'Chair',
-        image: Shivprasad,
-        linkedinURL: "https://www.linkedin.com/in/shivprasad-rao-allur-bb5568154/"
-    },
-    {
-        id: 2,
-        name: 'Shruti Mehta',
-        role: 'Vice Chair',
-        image: Shruti,
-        linkedinURL: "https://www.linkedin.com/in/shruti-mehta-821b871aa"
-    },
-    {
-        id: 3,
-        name: 'Sammeer Sawarkar',
-        role: 'Secretary',
-        image: Sameer,
-        linkedinURL: "https://www.linkedin.com/in/sameer-sawarkar"
-    },
-    {
-        id: 4,
-        name: 'Ankit Sonawane',
-        role: 'Treasurer',
-        image: Ankit,
-        linkedinURL: "https://www.linkedin.com/in/ankit-sonawane-24536321b"
-    },
-    {
-        id: 5,
-        name: 'Sanyukta Kamble',
-        role: 'Executive Manager',
-        image: Sanyukta,
-        linkedinURL: "https://www.linkedin.com/in/sanyukta-kamble-a158711a6"
-    },
-    {
-        id: 6,
         name: 'Omkar Danave',
-        role: 'Membership Chair',
+        role: 'Chair',
         image: Omkar,
         linkedinURL: "https://www.linkedin.com/in/omdanave/"
     },
     {
-        id: 7,
-        name: 'Aditya More',
-        role: 'WebMaster',
-        image: Aditya,
-        linkedinURL: "https://www.linkedin.com/in/aditya-more-a76b251b0/"
+        id: 2,
+        name: 'Anshika Singh',
+        role: 'Vice Chair',
+        image: "",
+        linkedinURL: "https://www.linkedin.com/in/anshika-singh-374612212/"
     },
+    {
+        id: 3,
+        name: 'Varun Agarwal',
+        role: 'Secretary',
+        image: "",
+        linkedinURL: "https://www.linkedin.com/in/varunagrawal408/"
+    },
+    {
+        id: 4,
+        name: 'Yash Yegare',
+        role: 'Treasurer',
+        image: Yash,
+       linkedinURL: "https://www.linkedin.com/in/yashyegare/"
+    },
+    {
+        id: 5,
+        name: 'Aval Sonigara',
+        role: 'Administrator',
+        image: Aval,
+        linkedinURL: "https://www.linkedin.com/in/aval-sonigara-397718220"
+    },
+    // {
+    //     id: 6,
+    //     name: 'Omkar Danave',
+    //     role: 'Membership Chair',
+    //     image: Omkar,
+    //     linkedinURL: "https://www.linkedin.com/in/omdanave/"
+    // },
+    {
+        id: 6,
+        name: 'Rohit Lohar',
+        role: 'WebMaster',
+        image: Rohit,
+        linkedinURL:"https://www.linkedin.com/in/loharrohit45/"
+    },
+    {
+        id: 7,
+        name: 'Anurag Gargote',
+        role: 'Technical Head',
+        image: Anurag,
+        linkedinURL: "https://www.linkedin.com/in/anurag-gargote-205204200/"
+    },
+    // {
+    //     id: 9,
+    //     name: 'Shreyas Honrao',
+    //     role: 'Discord Web Admin',
+    //     image: Shreyas,
+    //     linkedinURL: "https://www.linkedin.com/in/shreyas-honrao-088111124"
+    // },
     {
         id: 8,
-        name: 'Hritik Sharma',
-        role: 'Technical Head',
-        image: Hrithik,
-        linkedinURL: "https://www.linkedin.com/in/hrithiksh28"
+        name: 'Saadgi Pandey',
+        role: 'Social Media Manager',
+        image: Saadgi,
+        linkedinURL: "https://www.linkedin.com/in/saadgi-pandey-ab9818224/"
     },
+    // {
+    //     id: 9,
+    //     name: 'Vaishnavi Ghatage',
+    //     role: 'Co-ordinator Head',
+    //     image: Vaishnavi,
+    //     linkedinURL: "https://www.linkedin.com/in/vaishnavi-ghatage-9b107a1b3"
+    // },
     {
         id: 9,
-        name: 'Shreyas Honrao',
-        role: 'Discord Web Admin',
-        image: Shreyas,
-        linkedinURL: "https://www.linkedin.com/in/shreyas-honrao-088111124"
-    },
-    {
-        id: 10,
-        name: 'Abhishek Chirme',
-        role: 'Social Media Manager',
-        image: Abhishek,
-        linkedinURL: "https://www.linkedin.com/in/abhishek-chirme-a94b49198/"
-    },
-    {
-        id: 11,
-        name: 'Vaishnavi Ghatage',
-        role: 'Co-ordinator Head',
-        image: Vaishnavi,
-        linkedinURL: "https://www.linkedin.com/in/vaishnavi-ghatage-9b107a1b3"
-    },
-    {
-        id: 12,
-        name: 'Ninad Shirsat',
-        role: 'Editor',
-        image: Ninad,
-        linkedinURL: "https://www.linkedin.com/mwlite/in/ninad-s-23164b115"
+        name: 'Akshita Ghanwat',
+        role: 'Documentation',
+        image: Akshita,
+        linkedinURL: "https://www.linkedin.com/in/akshita-ghanwat-b0989a218/"
     },
 
 ]
